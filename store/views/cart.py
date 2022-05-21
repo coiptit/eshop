4c868afb2bfcc85a2ffa33db3fad7478c2360a34
coiptit <phu742k@gmail.com>
2022-05-21 19:00:13 +0700

    first commit

---
+++ store/views/cart.py
@@ -0,0 +1,39 @@
+from django.shortcuts import render , redirect , HttpResponseRedirect
+
+from django.contrib.auth.hashers import  check_password
+from store.models.customer import Customer
+from django.views import  View
+from store.models.product import Products
+
+class Cart(View):
+    def post(self , request):
+        product = request.POST.get('product')
+        remove = request.POST.get('remove')
+        cart = request.session.get('cart')
+        if cart:
+            quantity = cart.get(product)
+            if quantity:
+                if remove:
+                    if quantity<=1:
+                        cart.pop(product)
+                    else:
+                        cart[product]  = quantity-1
+                else:
+                    cart[product]  = quantity+1
+
+            else:
+                cart[product] = 1
+        else:
+            cart = {}
+            cart[product] = 1
+
+        request.session['cart'] = cart
+        print('cart' , request.session['cart'])
+        return redirect('cart')
+    def get(self , request):
+        ids = list(request.session.get('cart').keys())
+        products = Products.get_products_by_id(ids)
+        print(products)
+        return render(request , 'cart.html' , {'products' : products} )
+    
+
